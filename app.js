7a303df2d241f2f853357691181b238d28cf6445
Russell Perkins <dsw.ninjaboy@gmail.com>
2015-08-31 15:01:26 -0500

    heroku support

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// views as directory for all template files
+// Set port
+app.set('port', (process.env.PORT || 4000));
+// Views as directory for all template files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade'); // use either jade or ejs       
-// instruct express to server up static assets
+// Instruct express to server up static assets
 app.use(express.static('public'));
-// set routes
+// Set routes
 app.get('/', function(req, res) {
   res.render('index');
 });
-// Set server port
-app.listen(4000);
-
-console.log('server is running');
+// Main
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
