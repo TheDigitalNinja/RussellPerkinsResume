3fbb05faeddc9eaec535e4c92b8c99bfaa28e370
Russell Perkins <dsw.ninjaboy@gmail.com>
2015-09-07 18:13:36 -0600

    prep for integration testing

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-// set variables for environment
 var express = require('express');
 var app = express();
 var path = require('path');
 
 // Set port
 app.set('port', (process.env.PORT || 4000));
-// Views as directory for all template files
+
+// Views
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade'); // use either jade or ejs       
-// Instruct express to server up static assets
+app.set('view engine', 'jade');
+
+// Static Assets
 app.use(express.static('public'));
-// Set routes
+
+// Routes
 app.get('/', function(req, res) {
   res.render('index');
 });
+
 // Main
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+module.exports = app;
