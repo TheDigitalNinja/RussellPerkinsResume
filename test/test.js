27f7cf9869962b6adf11bcfa0e76978c04bdbf5a
Russell Perkins <dsw.ninjaboy@gmail.com>
2015-10-04 07:46:51 +0000

    added external resources tests

--- test/test.js
+++ test/test.js
@@ -17,3 +17,24 @@ describe('GET /css/style.css', function() {
     request(app).get('/css/style.css').expect(200,done)
   });
 });
+
+describe('GET /js/scripts.js', function() {
+  it('Should be status code 200', function(done) {
+    request(app).get('/js/scripts.js').expect(200,done)
+  });
+});
+
+describe('GET external resources', function() {
+  it('jquery', function(done) {
+    request('https://code.jquery.com').get('/jquery-2.1.4.min.js').expect(200,done)
+  });
+  it('bootstrap js', function(done) {
+    request('https://maxcdn.bootstrapcdn.com').get('/bootstrap/3.3.5/js/bootstrap.min.js').expect(200,done)
+  });
+  it('bootstrap css', function(done) {
+    request('https://maxcdn.bootstrapcdn.com').get('/bootstrap/3.3.5/css/bootstrap.min.css').expect(200,done)
+  });
+  it('FontAwesome', function(done) {
+    request('https://maxcdn.bootstrapcdn.com').get('/font-awesome/4.4.0/css/font-awesome.min.css').expect(200,done)
+  });
+});
