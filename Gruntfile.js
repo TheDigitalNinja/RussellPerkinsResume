62e3b7bc536a8ddd33082cc0657ba88883b79fdf
Russell Perkins <dsw.ninjaboy@gmail.com>
2015-10-07 06:18:20 +0000

    added grunt and sass support

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'public/css/style.css' : 'sass/style.sass'
+				}
+			}
+		},
+		exec: {
+			mocha: {
+				command: './node_modules/mocha/bin/mocha'
+			}
+		},
+		watch: {
+			options: {
+				livereload: true,
+			},
+			css: {
+				files: '**/*.sass',
+				tasks: ['sass'],
+			},
+			exec: {
+				files: ['**/*.js', '**/*.jade'],
+				tasks: ['exec']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
