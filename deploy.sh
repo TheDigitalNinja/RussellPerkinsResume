19f77b3c60c5695cd9cec38950ce8765707b2fc6
Russell Perkins <dsw.ninjaboy@gmail.com>
2015-10-14 16:29:39 +0000

    only add gzip headers to the gziped files

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./build.sh && aws s3 sync dist/ s3://russellperkins.net --content-encoding 'gzip' --delete --cache-control 'max-age=86400'
+./build.sh 
+
+aws s3 sync dist/ s3://russellperkins.net --exclude '*.html' --exclude '*.js' --exclude '*.css' --exclude '*.map' --cache-control 'max-age=86400'
+
+aws s3 cp dist/*.html s3://russellperkins.net --content-encoding 'gzip' --cache-control 'max-age=86400'
+aws s3 sync dist/js/ s3://russellperkins.net/js/ --content-encoding 'gzip' --cache-control 'max-age=86400'
+aws s3 sync dist/css/ s3://russellperkins.net/css/ --content-encoding 'gzip' --cache-control 'max-age=86400'
