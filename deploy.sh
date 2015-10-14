4f963cea2b88b1a2b489c5925135982f5b09176a
Russell Perkins <dsw.ninjaboy@gmail.com>
2015-10-14 15:36:00 +0000

    increased caching time

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-./build.sh && aws s3 sync dist/ s3://russellperkins.net --content-encoding 'gzip' --delete --cache-control 'max-age=3600'
-#aws s3 sync public/ s3://russellperkins.net --exclude '*.gz'
-#aws s3 sync public/ s3://russellperkins.net --exclude '*' --include '*.gz' --content-encoding 'gzip'
-
+./build.sh && aws s3 sync dist/ s3://russellperkins.net --content-encoding 'gzip' --delete --cache-control 'max-age=86400'
